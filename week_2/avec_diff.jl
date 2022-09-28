5fe33816dd4de053886b63b7ed18cd32c9ae1a47
Caspar <cgutsche@ethz.ch>
2022-09-28 13:27:15 +0200

    seperate step into function

--- week_2/avec_diff.jl
+++ week_2/avec_diff.jl
@@ -7,6 +7,15 @@ using ProgressMeter
 import Logging
 Logging.disable_logging(Logging.Warn)
 
+function finite_step!(C,q,dt,dx,vx,dc)
+    # advection
+    C[2:end  ] .-= dt.*max(vx,0.0).*diff(C)./dx # always take the derivative in the direction of movement
+    C[1:end-1] .-= dt.*min(vx,0.0).*diff(C)./dx
+    # diffusion
+    q          .= -dc.*diff(C)./dx
+    C[2:end-1] .-=   dt.*diff(q)./dx
+end
+
 function avec_diff(ic::Function,lx,dc,vx,ttot)
     # Nummerics
     nx = 200
@@ -21,19 +30,14 @@ function avec_diff(ic::Function,lx,dc,vx,ttot)
     C0 = ic.(x); C = copy(C0)
     q = zeros(nx-1)
 
-    p = Progress(nt, 1)
-    @animate for i in 1:nt
-        # advection
-        C[2:end  ] .-= dt.*max(vx,0.0).*diff(C)./dx # always take the derivative in the direction of movement
-        C[1:end-1] .-= dt.*min(vx,0.0).*diff(C)./dx
-        # diffusion
-        q          .= -dc.*diff(C)./dx
-        C[2:end-1] .-=   dt.*diff(q)./dx
+    p = Progress(nt, 0.2)
+    anim = @animate for i in 1:nt
+        finite_step!(C,q,dt,dx,vx,dx)
         # visualisation
         plot(x,C,label="concentration")
         next!(p)
     end every nvis
-    
+    return C,C0,x,anim
 end
 
 # Physics
@@ -42,5 +46,6 @@ dc   = 0.1   # diffusion coefficient
 vx   = 1.0   # advection velocity
 ttot = 20.0  # total simulation time
 gauss(x) = exp(-0.5*(x-lx/4)^2)
-anim = avec_diff(gauss,lx,dc,vx,ttot)
+Cfinal,C0,x,anim = avec_diff(gauss,lx,dc,vx,ttot)
 gif(anim,"week_2/tmp/advection_diff.gif",fps=15)
+
