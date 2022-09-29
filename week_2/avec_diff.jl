ce84ef4c6602c6737f99cf990363d62c1d6e2e3c
Caspar <cgutsche@ethz.ch>
2022-09-29 16:52:11 +0200

    rewrite with main function

--- week_2/avec_diff.jl
+++ week_2/avec_diff.jl
@@ -8,6 +8,7 @@ import Logging
 Logging.disable_logging(Logging.Warn)
 
 function finite_step!(C,q,dt,dx,vx,dc)
+    # I am a little confused about what boundary conditions to use
     # advection
     C[2:end  ] .-= dt.*max(vx,0.0).*diff(C)./dx # always take the derivative in the direction of movement
     C[1:end-1] .-= dt.*min(vx,0.0).*diff(C)./dx
@@ -19,13 +20,12 @@ end
 function avec_diff(ic::Function,lx,dc,vx,ttot)
     # Nummerics
     nx = 200
-    nt = 2000
     dx = lx/nx
     dta  = dx/abs(vx)
     dtd  = dx^2/dc/2
-    dt   = min(dtd, dta) #it works if i decrease timesteps by 10
+    dt   = min(dtd, dta)
+    nt = Int(round(ttot/dt))
     nvis = 3
-
     x = range(0,lx,nx)
     C0 = ic.(x); C = copy(C0)
     q = zeros(nx-1)
@@ -34,18 +34,26 @@ function avec_diff(ic::Function,lx,dc,vx,ttot)
     anim = @animate for i in 1:nt
         finite_step!(C,q,dt,dx,vx,dx)
         # visualisation
-        plot(x,C,label="concentration")
+        plot(x,C,label="concentration at t=$(round(dt*i,digits=1))",xlabel="distance",ylabel="concentration")
+        plot!(x,C0,label="inital concentration")
         next!(p)
     end every nvis
     return C,C0,x,anim
 end
 
-# Physics
-lx   = 20.0  # domain length
-dc   = 0.1   # diffusion coefficient
-vx   = 1.0   # advection velocity
-ttot = 20.0  # total simulation time
-gauss(x) = exp(-0.5*(x-lx/4)^2)
-Cfinal,C0,x,anim = avec_diff(gauss,lx,dc,vx,ttot)
-gif(anim,"week_2/tmp/advection_diff.gif",fps=15)
+function main()
+    # Physics
+    lx   = 20.0  # domain length
+    dc   = 0.1   # diffusion coefficient
+    vx   = 1.0   # advection velocity
+    ttot = 20.0  # total simulation time
+    gauss(x) = exp(-(x-lx/4)^2)
+    Cfinal,C0,x,anim = avec_diff(gauss,lx,dc,vx,ttot)
+    anim
+end
 
+anim = main()
+gif(anim,"week_2/tmp/advection_diff.gif",fps=15)
+m,n = findmax(Cfinal)
+plot(x,Cfinal,label="final concentration  (max:$(round(m,digits=2)) at $(x[n])",xlabel="distance",ylabel="concentration")
+plot!(x,C0,label="inital concentration",xlabel="distance",ylabel="concentration")
