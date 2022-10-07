6cfb8d3b17bb06a8475751f1c92667d780e4df61
Caspar <cgutsche@ethz.ch>
2022-10-07 21:42:42 +0200

    flesh out acoustic 2D, still diverges unfortuntaly

--- week_3/acoustic_2D_v1.jl
+++ week_3/acoustic_2D_v1.jl
@@ -1,6 +1,8 @@
 using Plots
 #imports a nice progress bar
 using ProgressMeter
+using PropertyUtils
+
 
 # supress warnings about No strict ticks found
 import Logging
@@ -8,31 +10,60 @@ Logging.disable_logging(Logging.Warn)
 
 abstract type PDE_system end
 
+
 Base.@kwdef mutable struct Acoustic <: PDE_system
+    # physics
+    c = 1
+    ρ = (c/2)^2
+    σ = (c/2)^2
+    # inital condition
+    ic = (x,y) -> exp(-(x^2+ y^2))
+    Lx = 10
+    Ly = 10
+    # nummerics
     nx = 128
     ny = 129
-end    
-
-
+    dx = Lx/nx
+    dy = Ly/ny
+    dt = min(dx,dy) /c
+    # derived nummerics
+    x = range(-Lx/2,Lx/2,nx)
+    y = range(-Ly/2,Ly/2,ny)
+    Vx = zeros(nx-1,ny)
+    Vy = zeros(nx,ny-1)
+    P0 = ic.(x,y')
+    P = copy(P0)
+end
 
 function finite_step!(s::PDE_system)
-    # inviscid Burgers
-    s.C[2:end] .+= -  max(s.v,0.) .* s.dt.*diff(s.C.^s.n)./s.dx
-    s.C[1:end-1] .+= -  min(s.v,0) .* s.dt.*diff(s.C.^s.n)./s.dx
+    # wave equation
+    @infiltrate
+    @with s begin
+        s.Vx .+= dt .* diff(P,dims=1)./dx ./ρ
+        s.Vy .+= dt .* diff(P,dims=2)./dx ./ρ
+        s.P[2:end-1,:] .+= dt .* diff(Vx,dims=1) ./ dx ./σ
+        s.P[:,2:end-1] .+= dt .* diff(Vy,dims=2) ./ dx ./σ
+    end 
 end
 
-"modefies the Setup type in the input so that it contains the solution in the Setup.C field"
-function solve_pde!(s::PDE_system)
-p = Progress(s.nt, 0.2)
-    anim = @animate for i in 1:s.nt
+"modefies the PDE_system in the input so that it contains the solution in corrsponding field"
+function solve_pde!(s::PDE_system,anim_time,ttot)
+    fps = 15
+    nt = Int(ceil(ttot/s.dt))
+    nvis = Int(ceil(nt/(anim_time*fps)))
+    p = Progress(nt, 0.2)
+    anim = @animate for i in 1:nt
         finite_step!(s)
         # visualisation
-        plot(s.xc,s.C,label="concentration at t=$(round(s.dt*i,digits=1))",xlabel="distance",ylabel="concentration")
-        plot!(s.xc,s.C0,label="inital concentration")
+        heatmap(s.x,s.y,s.P)
+        @infiltrate
         next!(p)
-        if i > s.nx #change velocyity after half the time
-            s.v = -1.
-        end
-    end every s.nvis
+    end every nvis
     return anim
 end
+
+# for testing
+s = Acoustic()
+finite_step!(s)
+an = solve_pde!(s,4,20)
+gif(an,fps=15)
