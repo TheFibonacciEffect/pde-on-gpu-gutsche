315d84f166102c96d988ea48b1f2a30aa0073cc9
Caspar <cgutsche@ethz.ch>
2022-10-10 22:53:18 +0200

    stuff does not converge

--- lecture3/l3e3 Advection-diffusion in 2D.jl
+++ lecture3/l3e3 Advection-diffusion in 2D.jl
@@ -5,52 +5,44 @@ default(framestyle=:box,label=false,grid=false,margin=10mm,lw=6,labelfontsize=20
 
 @views function steady_diffusion_1D()
     # physics
-    lx,ly   = 10.0,10.0
+    lx   = 10.0
     dc      = 1.0
     vx      = 10.0
-    vy      = -10.0
     da      = 1000.0
     re      = π + sqrt(π^2 + da)
     ρ       = (lx/(dc*re))^2 #how does ly factor into this?
     # numerics
-    nx,ny   = 200,201
+    nx   = 200
     ϵtol    = 1e-8
     maxiter = 10nx
     ncheck  = ceil(Int,0.02nx)
     nt      = 50
     # derived numerics
     dx      = lx/nx
-    dy      = ly/ny
-    dt = min(dx/abs(vx),dy/abs(vy))/2
+    dt       = lx^2/dc/da
     xc      = LinRange(dx/2,lx-dx/2,nx)
-    yc      = LinRange(dy/2,ly-dy/2,ny)
-    dτ = min(dx,dy)/sqrt(1/ρ)/sqrt(2)
+    dτ      = dx/sqrt(1/ρ)
     # array initialisation
-    C       = @. exp(-(xc-lx/4)^2 -(yc'-3ly/4)^2)
+    C       = @. 1.0 + exp(-(xc-lx/4)^2) - xc/lx; C_i = copy(C)
     C_old   = copy(C) 
-    qx      = zeros(nx-1,ny)
-    qy      = zeros(nx,ny-1)
+    qx      = zeros(nx-1)
     # other
     p = Progress(nt, 1)
     anim = @animate for it = 1:nt
         C_old .= C
         # why does this produce such a strange plot?
         # p1 = heatmap(xc,yc,C;xlabel="lx",ylabel="ly",title="Implicit transient diffusion using dual timestepping")
-        p1 = heatmap(C;xlabel="lx",ylabel="ly",title="Implicit transient diffusion using dual timestepping")
+        p1 = plot(xc,C,title="please help! D:")
         # iteration loop
         iter = 1; err = 2ϵtol; iter_evo = Float64[]; err_evo = Float64[]
         while err >= ϵtol && iter <= maxiter
             qx           .-= dτ./(ρ   .+ dτ/dc).*(qx./dc                                 .+ diff(C,dims=1) ./dx)
-            qy           .-= dτ./(ρ   .+ dτ/dc).*(qy./dc                                 .+ diff(C,dims=2) ./dy)
-            diffxC = dτ./(1.0 .+ dτ/dt).*((C[2:end-1,:] .- C_old[2:end-1,:])./dt .+ diff(qx,dims=1)./dx)
-            diffyC = dτ./(1.0 .+ dτ/dt).*((C[:,2:end-1] .- C_old[:,2:end-1])./dt .+ diff(qy,dims=2)./dy)
-            C[2:end-1,:] .-= diffxC
-            C[:,2:end-1] .-= diffyC
+            diffxC = dτ./(1.0 .+ dτ/dt).*((C[2:end-1] .- C_old[2:end-1])./dt .+ diff(qx,dims=1)./dx)
+            C[2:end-1] .-= diffxC
             println(sqrt.(sum((dτ./(1.0 .+ dτ/dt).*((C[2:end-1,:] .- C_old[2:end-1,:])./dt .+ diff(qx,dims=1)./dx)).^2)))
             if iter%ncheck == 0
-                △yC = (diff(dc.*diff(C,dims=2)./dy,dims=2)./dy)[2:end-1,:]
-                △xC = (diff(dc.*diff(C,dims=1)./dx,dims=1)./dx)[:,2:end-1]
-                err = maximum(abs.(△yC .+ △xC .- (C[2:end-1,2:end-1] .- C_old[2:end-1,2:end-1])./dt))
+                △xC = (diff(dc.*diff(C,dims=1)./dx,dims=1)./dx)
+                err = maximum(abs.(△xC .- (C[2:end-1] .- C_old[2:end-1])./dt))
                 push!(iter_evo,iter/nx); push!(err_evo,err)
             end
             iter += 1
