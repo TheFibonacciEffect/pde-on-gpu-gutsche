d2b12f58d09fc2a677077f228eb88728fbda45a7
Caspar <cgutsche@ethz.ch>
2022-10-15 20:35:22 +0200

    base file

--- lecture4/scripts/l4 porpus convection 2D.jl
+++ lecture4/scripts/l4 porpus convection 2D.jl
@@ -1,39 +1,41 @@
 using Plots,Plots.Measures,Printf
-default(size=(1200,800),framestyle=:box,label=false,grid=false,margin=10mm,lw=6,labelfontsize=20,tickfontsize=20,titlefontsize=24)
+default(size=(600,500),framestyle=:box,label=false,grid=false,margin=10mm,lw=6,labelfontsize=11,tickfontsize=11,titlefontsize=11)
 
-@views function porous_convection_2D()
+function Pf_diffusion_2D()
     # physics
-    lx      = 20.0
-    dc      = 1.0
-    ρ       = (lx/(dc*2π))^2
+    lx,ly   = 20.0,20.0
+    k_ηf    = 1.0
     # numerics
-    nx      = 100
+    nx,ny   = 127,127
     ϵtol    = 1e-8
-    maxiter = 100nx
-    ncheck  = ceil(Int,0.25nx)
+    maxiter = 10max(nx,ny)
+    ncheck  = ceil(Int,0.25max(nx,ny))
+    cfl     = 1.0/sqrt(2.1)
+    re      = 2π
     # derived numerics
-    dx      = lx/nx
-    xc      = LinRange(dx/2,lx-dx/2,nx)
-    dτ      = dx/sqrt(1/ρ)
+    dx,dy   = lx/nx,ly/ny
+    xc,yc   = LinRange(dx/2,lx-dx/2,nx),LinRange(dy/2,ly-dy/2,ny)
+    θ_dτ    = max(lx,ly)/re/cfl/min(dx,dy)
+    β_dτ    = (re*k_ηf)/(cfl*min(dx,dy)*max(lx,ly))
     # array initialisation
-    C       = @. 1.0 + exp(-(xc-lx/4)^2) - xc/lx; C_i = copy(C)
-    qx      = zeros(Float64, nx-1)
+    Pf      = @. exp(-(xc-lx/2)^2 -(yc'-ly/2)^2)
+    qDx,qDy = zeros(Float64, nx+1,ny),zeros(Float64, nx,ny+1)
+    r_Pf    = zeros(nx,ny)
     # iteration loop
-    iter = 1; err = 2ϵtol; iter_evo = Float64[]; err_evo = Float64[]
-    while err >= ϵtol && iter <= maxiter
-        qx         .-= dτ./(ρ*dc .+ dτ).*(qx .+ dc.*diff(C)./dx)
-        C[2:end-1] .-= dτ.*diff(qx)./dx
+    iter = 1; err_Pf = 2ϵtol
+    while err_Pf >= ϵtol && iter <= maxiter
+        qDx[2:end-1,:] .-= (qDx[2:end-1,:] .+ k_ηf.*(diff(Pf,dims=1)./dx))./(1.0 + θ_dτ)
+        qDy[:,2:end-1] .-= (qDy[:,2:end-1] .+ k_ηf.*(diff(Pf,dims=2)./dy))./(1.0 + θ_dτ)
+        Pf             .-= (diff(qDx,dims=1)./dx .+ diff(qDy,dims=2)./dy)./β_dτ
         if iter%ncheck == 0
-            err = maximum(abs.(diff(dc.*diff(C)./dx)./dx))
-            push!(iter_evo,iter/nx); push!(err_evo,err)
-            p1 = plot(xc,[C_i,C];xlims=(0,lx), ylims=(-0.1,2.0),
-                      xlabel="lx",ylabel="Concentration",title="iter/nx=$(round(iter/nx,sigdigits=3))")
-            p2 = plot(iter_evo,err_evo;xlabel="iter/nx",ylabel="err",
-                      yscale=:log10,grid=true,markershape=:circle,markersize=10)
-            display(plot(p1,p2;layout=(2,1)))
+            r_Pf  .= diff(qDx,dims=1)./dx .+ diff(qDy,dims=2)./dy
+            err_Pf = maximum(abs.(r_Pf))
+            @printf("  iter/nx=%.1f, err_Pf=%1.3e\n",iter/nx,err_Pf)
+            display(heatmap(xc,yc,Pf';xlims=(xc[1],xc[end]),ylims=(yc[1],yc[end]),aspect_ratio=1,c=:turbo,clim=(0,1)))
         end
         iter += 1
     end
+    return
 end
 
-porous_convection_2D()
+Pf_diffusion_2D()
