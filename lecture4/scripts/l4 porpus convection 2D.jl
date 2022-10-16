7eef0618bc4a416a5ddad2155e23f131e86f47d7
Caspar <cgutsche@ethz.ch>
2022-10-16 19:30:19 +0200

    l4e1 1D

--- lecture4/scripts/l4 porpus convection 2D.jl
+++ lecture4/scripts/l4 porpus convection 2D.jl
@@ -1,16 +1,14 @@
 using Plots,Plots.Measures,Printf
 using ProgressMeter
-default(size=(1200,800),framestyle=:box,label=false,grid=false,margin=10mm,lw=6,labelfontsize=20,tickfontsize=20,titlefontsize=24)
+default(size=(1200,600*3),framestyle=:box,label=false,grid=false,margin=10mm,lw=6,labelfontsize=20,tickfontsize=20,titlefontsize=24)
 
-@views function porous_convection_2D()
+@views function porous_convection_2D(bounary)
     # physics
-    lx      = 20.0
+    lx      = 40.0
     ly      = 20.0
-    k = 1.
-    η = 1.
     # numerics
     nx      = 100
-    ny      = 101
+    ny      = 200
     ϵtol    = 1e-8
     maxiter = 100nx
     ncheck  = ceil(Int,0.25nx)
@@ -20,6 +18,7 @@ default(size=(1200,800),framestyle=:box,label=false,grid=false,margin=10mm,lw=6,
     dx      = lx/nx
     dy      = ly/ny
     xc      = LinRange(dx/2,lx-dx/2,nx)
+    yc      = LinRange(dy/2,ly-dy/2,ny)
     k_ηnf = 1 #TODO
     θ_dt =max(lx,ly)/re/cfl/max(dx,dy)
     β_dt = (re*k_ηnf)/(cfl*dx*lx)
@@ -30,8 +29,10 @@ default(size=(1200,800),framestyle=:box,label=false,grid=false,margin=10mm,lw=6,
     iter = 1; err_Pf = 2ϵtol; iter_evo = Float64[]; err_evo = Float64[]
     while err_Pf >= ϵtol && iter <= maxiter
         # boundary conditions on the flux
-        qDx[1]   = 0
-        qDx[end] = 0
+        if bounary
+            qDx[1]   = 0
+            qDx[end] = 0
+        end
         # differential equation
         qDx .-= (qDx .+ k_ηnf .* diff(Pf)./dx)./(θ_dt .+ 1)
         Pf[2:end-1] .-= (diff(qDx)./dx)./β_dt
@@ -44,9 +45,9 @@ default(size=(1200,800),framestyle=:box,label=false,grid=false,margin=10mm,lw=6,
         iter += 1
     end
     p1 = plot(xc,[Pf,Pfi])
-    p2 = plot(iter_evo,err_evo;xlabel="iter/nx",ylabel="err",
-              yscale=:log10,grid=true,markershape=:circle,markersize=10)
-    display(plot(p1,p2;layout=(2,1)))
+    p2 = plot(iter_evo,err_evo;xlabel="iter/nx",ylabel="err",yscale=:log10,grid=true,markershape=:circle,markersize=10)
+    p3 = plot(xc[1:end-1],qDx,title="Flux qDx")
+    display(plot(p1,p2,p3;layout=(3,1)))
 end
 
-porous_convection_2D()
+porous_convection_2D(true)
