49e3046bc872e9c66d3080819fa5bda8aa56445a
Caspar <cgutsche@ethz.ch>
2022-10-19 13:07:48 +0200

    Ra was 100 instead of 1000

--- lecture4/scripts/l4 porpus convection 2D.jl
+++ lecture4/scripts/l4 porpus convection 2D.jl
@@ -16,7 +16,7 @@ default(size=(600*2,600*3),framestyle=:box,label=false,grid=false,margin=10mm,lw
     αρg       = sqrt(αρgx^2+αρgy^2)
     ΔT        = 200.0
     ϕ         = 0.1
-    Ra        = 100
+    Ra        = 1000
     λ_ρCp     = 1/Ra*(αρg*k_ηf*ΔT*ly/ϕ) # Ra = αρg*k_ηf*ΔT*ly/λ_ρCp/ϕ
     # numerics
     nx      = 100
