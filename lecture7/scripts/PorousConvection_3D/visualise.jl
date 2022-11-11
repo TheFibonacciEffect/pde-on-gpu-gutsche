5c329eabdd1e9f9bf4500cc7dc4bbbeb78f143e8
Caspar <cgutsche@ethz.ch>
2022-11-11 17:40:46 +0100

    now visualize detects the parameters from the name

--- lecture7/scripts/PorousConvection_3D/visualise.jl
+++ lecture7/scripts/PorousConvection_3D/visualise.jl
@@ -7,11 +7,12 @@ end
 
 function visualise()
     lx,ly,lz = 40.0,20.0,20.0
-    nz = 63
-    ny = nz
-    nx = 2*(nz+1)-1
+    aname = "out_T-255-127-127-20"
+    _, nx,ny,nz,_ = split(aname,"-")
+    nx = parse(Int,nx); ny = parse(Int,ny); nz = parse(Int,nz)
     T  = zeros(Float32,nx,ny,nz)
-    load_array("bin\\out_T",T)
+    load_array(aname,T)
+    @assert maximum(T) != 0.0
     xc,yc,zc = LinRange(0,lx,nx),LinRange(0,ly,ny),LinRange(0,lz,nz)
     fig      = Figure(resolution=(1600,1000),fontsize=24)
     ax       = Axis3(fig[1,1];aspect=(1,1,0.5),title="Temperature",xlabel="lx",ylabel="ly",zlabel="lz")
