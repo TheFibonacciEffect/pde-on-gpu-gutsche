82c062d93c3635bae1b19014838b089f28fb2bc9
Caspar <cgutsche@ethz.ch>
2022-11-12 10:43:17 +0100

    gitignore includes bin

--- lecture7/scripts/PorousConvection_3D/get_result.bash
+++ lecture7/scripts/PorousConvection_3D/get_result.bash
@@ -1,1 +1,2 @@
 scp "class202@daint:~/github-repo/pde-on-gpu-gutsche/lecture7/scripts/PorousConvection_3D/*.bin" .
+scp -r "class202@daint:~/github-repo/pde-on-gpu-gutsche/lecture7/scripts/PorousConvection_3D/*_out/" .
