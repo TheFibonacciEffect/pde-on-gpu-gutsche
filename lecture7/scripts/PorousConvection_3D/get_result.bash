c8531b93693aa4025ac0725b77ef1fd1150a8dc9
Caspar <cgutsche@ethz.ch>
2022-11-15 14:44:22 +0100

    get out and error files

--- lecture7/scripts/PorousConvection_3D/get_result.bash
+++ lecture7/scripts/PorousConvection_3D/get_result.bash
@@ -1,2 +1,4 @@
 scp "class202@daint:~/github-repo/pde-on-gpu-gutsche/lecture7/scripts/PorousConvection_3D/*.bin" .
 scp -r "class202@daint:~/github-repo/pde-on-gpu-gutsche/lecture7/scripts/PorousConvection_3D/*_out/" .
+scp "class202@daint:~/github-repo/pde-on-gpu-gutsche/lecture7/scripts/PorousConvection_3D/*.o/" .
+scp "class202@daint:~/github-repo/pde-on-gpu-gutsche/lecture7/scripts/PorousConvection_3D/*.e/" .
