962dd76d7490e5becf9628bbe85adf71c3ff7c8a
Caspar <cgutsche@ethz.ch>
2022-11-16 15:37:49 +0100

    get the output from 2d and 3d code

--- lecture7/scripts/PorousConvection_2D/create_gif.bash
+++ lecture7/scripts/PorousConvection_2D/create_gif.bash
@@ -2,5 +2,5 @@ interval=20
 figs_path="../../figs"
 imgs_path="./PorousConvection_2D_xpu_daint_out"
 # cd $imgs_path
-convert -delay 3 -loop 0 $imgs_path/*.png "$figs_path/out.gif"
+convert -delay 5 -loop 0 $imgs_path/*.png "$figs_path/out.gif"
 # convert -delay 10 -loop 0 *.png out.gif
--- lecture7/scripts/PorousConvection_2D/get_files.bash
+++ lecture7/scripts/PorousConvection_2D/get_files.bash
@@ -1,4 +1,4 @@
 if [ ! -d "./PorousConvection_2D_xpu_daint_out" ]; then
   mkdir -p "./PorousConvection_2D_xpu_daint_out";
 fi
-scp -r class202@daint:/users/class202/github-repo/pde-on-gpu-gutsche/lecture7/scripts/PorousConvection_2D/PorousConvection_2D_xpu_daint_out "./PorousConvection_2D_xpu_daint_out"
+scp -r class202@daint:/users/class202/github-repo/pde-on-gpu-gutsche/lecture7/scripts/PorousConvection_2D/PorousConvection_2D_xpu_daint_out "."
