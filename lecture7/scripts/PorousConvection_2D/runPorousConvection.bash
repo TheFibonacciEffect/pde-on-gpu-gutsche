a3c365169951d57a98027ba5840a5c2dd248f579
Caspar <cgutsche@ethz.ch>
2022-11-16 10:13:24 +0100

    2D parameters reflect exercise

--- lecture7/scripts/PorousConvection_2D/runPorousConvection.bash
+++ lecture7/scripts/PorousConvection_2D/runPorousConvection.bash
@@ -12,4 +12,4 @@
 
 module load daint-gpu
 module load Julia/1.7.2-CrayGNU-21.09-cuda
-srun julia -O3 --check-bounds=no --project=../.. ./PorousConvection_2D_xpu_daint.jl 511 1023 40
+srun julia -O3 --check-bounds=no --project=../.. ./PorousConvection_2D_xpu_daint.jl 511 1023 4000
