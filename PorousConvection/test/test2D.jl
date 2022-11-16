78dc0a6cb42529d80dbe9ec29027a294ff679877
Sidney Beeler <sidney.beeler@gmail.com>
2022-11-16 14:47:12 +0100

    Added unit tests on boundary condition funtion in both 2D,3D

--- PorousConvection/test/test2D.jl
+++ PorousConvection/test/test2D.jl
@@ -12,8 +12,16 @@ include("../src/PorousConvection_2D_xpu_daint.jl")
 ny=50;nx=50;nt=10
 T = PorousConvection_2D_xpu.porous_convection_2D(ny,nx,nt;do_vis=false)
 
+#test boundary conditions
+@testset "Test BC" begin
+    @test all(T[1,:] .== T[2,:])
+    @test all(T[end,:] .== T[end-1,:])
+end
+
 # compare arrays
 T_ref = zeros(Float32,nx,ny)
 load_array("../bins/out_T_2D-$(nx)-$(ny)-$(nt)",T_ref)
-@test T_ref ≈ T
+@testset "Reference Test" begin
+    @test T_ref ≈ T
+end
 
--- PorousConvection/test/test3D.jl
+++ PorousConvection/test/test3D.jl
@@ -15,8 +15,19 @@ nx          = Int(ceil(255/127*nz))
 nt = 10
 T = PorousConvection_3D_xpu.porous_convection_3D(;nz=nz,nt=nt,do_vis=false,save_arr=false)
 
+#test boundary conditions
+@testset "Test BC" begin
+    @test all(T[1,:,:] .== T[2,:,:])
+    @test all(T[:,end,:] .== T[:,end-1,:])
+    @test all(T[:,1,:] .== T[:,2,:])
+    @test all(T[end,:,:] .== T[end-1,:,:])
+end
+
 # compare arrays
 T_ref = zeros(Float32,nx,ny,nz)
 load_array("../bins/out_T-$(nx)-$(ny)-$(nz)-$(nt)",T_ref)
-@test T_ref ≈ T
+#test boundary conditions
+@testset "Reference Test" begin
+    @test T_ref ≈ T
+end
 
