2231333975944fcc6e68978550e18b23cca9333c
Caspar <cgutsche@ethz.ch>
2022-11-23 18:17:46 +0100

    get results

--- lecture8/src/ex2/get_results.bash
+++ lecture8/src/ex2/get_results.bash
@@ -1,1 +1,2 @@
 scp daint:/users/class202/github-repo/pde-on-gpu-gutsche/lecture8/src/ex2/diffusion_2D_xpu_gpu*.gif ../../docs/
+scp -r daint:/users/class202/github-repo/pde-on-gpu-gutsche/lecture8/docs/ ../../
