914caf4845e40ce18287e20ede7a82c9763729d7
Sidney Beeler <sidney.beeler@gmail.com>
2022-11-23 19:41:23 +0100

    NEW: added test file

---
+++ lecture8/test/runtests.jl
@@ -0,0 +1,18 @@
+using Test, MAT
+
+gputrue = []
+gpufalse = []
+mpigpu = []
+
+file = matopen("../docs/l8ex2t2/gputrue_out.mat")
+gputrue = read(file, "C"); close(file)
+file = matopen("../docs/l8ex2t2/gpufalse_out.mat")
+gpufalse = read(file, "C"); close(file)
+file = matopen("../docs/l8ex2t2/mpigpu_out.mat")
+mpigpu = read(file, "C"); close(file)
+
+#test boundary conditions
+@testset "Unit test" begin
+    @test all(gputrue ≈ gpufalse) 
+    @test all (gputrue ≈ mpigpu)
+end
