6da8a732940ce0019d8db7a3a0d04aa0bbdb1a8d
Caspar <cgutsche@ethz.ch>
2022-11-23 17:35:18 +0100

    bugfix l8

--- lecture8/src/ex2/run_task2.2.bash
+++ lecture8/src/ex2/run_task2.2.bash
@@ -11,5 +11,5 @@
 
 module load daint-gpu
 module load Julia/1.7.2-CrayGNU-21.09-cuda
-srun julia -O3 --check-bounds=no --project=../..  diffusion_2D_perf_xpu.jl true
-srun julia -O3 --check-bounds=no --project=../..  diffusion_2D_perf_xpu.jl false
+srun julia -O3 --check-bounds=no --project=../..  l8_diffusion_2D_perf_xpu.jl true
+srun julia -O3 --check-bounds=no --project=../..  l8_diffusion_2D_perf_xpu.jl false
