b850e704e1802d5fe4b08ef80fe47d425c286ba2
Caspar Gutsche <cgutsche@ethz.ch>
2022-11-24 10:55:00 +0100

    fix weak scaling job

--- lecture8/src/ex2/t5/weak_scaling_job.bash
+++ lecture8/src/ex2/t5/weak_scaling_job.bash
@@ -1,7 +1,7 @@
 #!/bin/bash -l
-#SBATCH --job-name="task2.3"
-#SBATCH --output=task2.3.%j.o
-#SBATCH --error=task2.3.%j.e
+#SBATCH --job-name="weak_scaling"
+#SBATCH --output=weak_scaling.%j.o
+#SBATCH --error=weak_scaling.%j.e
 #SBATCH --time=00:00:20
 #SBATCH --ntasks-per-node=1
 #SBATCH --partition=normal
@@ -14,5 +14,5 @@ module load Julia/1.7.2-CrayGNU-21.09-cuda
 export MPICH_RDMA_ENABLED_CUDA=1
 export IGG_CUDAAWARE_MPI=1
 
-srun -n4 bash -c 'LD_PRELOAD="/usr/lib64/libcuda.so:/usr/local/cuda/lib64/libcudart.so" julia -O3 --check-bounds=no --project=../../.. l8_diffusion_2D_perf_multixpu.jl'
+srun -n4 bash -c 'LD_PRELOAD="/usr/lib64/libcuda.so:/usr/local/cuda/lib64/libcudart.so" julia -O3 --check-bounds=no --project=../../.. weak_scaling.jl'
 srun julia --project=../../.. "plot result.jl"
