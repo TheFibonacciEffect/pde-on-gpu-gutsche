646f0d6f35f438194f17c682de9c2b762f3980bb
Caspar <cgutsche@ethz.ch>
2022-11-27 16:08:38 +0100

    fix: fix path when relocating

--- PorousConvection/src/3D multixpu/PorousConvection_3D_multixpu.jl
+++ PorousConvection/src/3D multixpu/PorousConvection_3D_multixpu.jl
@@ -115,7 +115,7 @@ end
     # vis
     if do_visu
         ENV["GKSwstype"]="nul"
-        if (me==0) if isdir("../docs/l9ex1t1")==false mkdir("../docs/l9ex1t1") end; loadpath="../docs/l9ex1t1"; anim=Animation(loadpath,String[]); println("Animation directory: $(anim.dir)") end
+        if (me==0) if isdir("../../docs/l9ex1t1")==false mkdir("../../docs/l9ex1t1") end; loadpath="../../docs/l9ex1t1"; anim=Animation(loadpath,String[]); println("Animation directory: $(anim.dir)") end
         nx_v,ny_v,nz_v = (nx-2)*dims[1],(ny-2)*dims[2],(nz-2)*dims[3]
         if (nx_v*ny_v*nz_v*sizeof(Data.Number) > 0.8*Sys.free_memory()) error("Not enough memory for visualization.") end
         T_v   = zeros(nx_v, ny_v, nz_v) # global array for visu
@@ -171,8 +171,8 @@ end
             if me==0
                 p1=heatmap(xi_g,zi_g,T_v[:,ceil(Int,ny_g()/2),:]';xlims=(xi_g[1],xi_g[end]),ylims=(zi_g[1],zi_g[end]),clims=(-100,100),aspect_ratio=1,c=:turbo)
                 # display(p1)
-                png(p1,@sprintf("../docs/l9ex1t1/PC_2D_$(lpad(it,4,"0")).mat.png",iframe+=1))
-                save_array(@sprintf("../docs/l9ex1t1/PC_3D_$(lpad(it,4,"0"))",iframe),convert.(Float32,T_v))
+                png(p1,@sprintf("../../docs/l9ex1t1/PC_2D_$(lpad(it,4,"0")).mat.png",iframe+=1))
+                save_array(@sprintf("../../docs/l9ex1t1/PC_3D_$(lpad(it,4,"0"))",iframe),convert.(Float32,T_v))
             end
         end
 
