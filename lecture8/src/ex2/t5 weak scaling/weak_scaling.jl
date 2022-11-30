7dd52e261a9a0455441bcdc86f726ad251dbd45c
Caspar <cgutsche@ethz.ch>
2022-11-30 16:33:43 +0100

    only main process writes to file

--- lecture8/src/ex2/t5 weak scaling/weak_scaling.jl
+++ lecture8/src/ex2/t5 weak scaling/weak_scaling.jl
@@ -23,9 +23,10 @@ include("../t3/l8_diffusion_2D_perf_multixpu.jl")
 t,me = diffusion_2D(; do_visu=false,do_save=false,ttot = 1)
 
 # write to file
-sleep(0.1*me)
-fname = "weak_scaling.dat"
-print("writing to file $(fname)...")
-fid = open(fname,"a") #append
-write(fid, string(ARGS[1],",",t,"\n"))
-close(fid)
+if me==0
+    fname = "weak_scaling.dat"
+    print("writing to file $(fname)...")
+    fid = open(fname,"a") #append
+    write(fid, string(ARGS[1],",",t,"\n"))
+    close(fid)
+end
