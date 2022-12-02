e3ff18d2660118e9a83a7461b7420ca84f4668ff
Caspar <cgutsche@ethz.ch>
2022-12-02 08:42:04 +0100

    hide communication

--- lecture8/src/ex2/t6 communication/time_communication.jl
+++ lecture8/src/ex2/t6 communication/time_communication.jl
@@ -14,6 +14,13 @@ include("../t3/l8_diffusion_2D_perf_multixpu.jl")
 times = []
 is = []
 js = []
+
+
+t_toc, me = diffusion_2D(; do_visu=false,do_save=true,hidecom=false)
+me == 0 && open("diffusion_2D_perf_multixpu.txt", "a") do f
+    println(f, "no hidecomm, t_toc = $(t_toc)")
+end
+
 for (i,j) in ([2,16,16],[2,4,16])
     t_toc, me = diffusion_2D(; do_visu=false,do_save=true)
     if me == 0
@@ -28,4 +35,4 @@ for (i,j) in ([2,16,16],[2,4,16])
 end
 
 # Plot
-plot(is, times, label="diffusion_2D_perf_multixpu", xlabel="i", ylabel="time (s)", title="diffusion_2D_perf_multixpu")
+plot(is, times, label="diffusion_2D_perf_multixpu", xlabel="i,j", ylabel="time (s)", title="diffusion_2D_perf_multixpu",xticks=[no-hidecomm, (2,2), (8,2), (16,4), (16,16)])
